6b268622333355f2da79ba3de96a40c9dec3c494
madison-boman <mrb475@nau.edu>
2019-01-30 23:02:56 -0700

    Finished up to Screencast 3

--- src/interface.cpp
+++ src/interface.cpp
@@ -14,4 +14,16 @@ void knn_interface(
   if(status != 0){
     error("non-zero exit status from knn");
   }
+}
+
+R_CMethodDef cMethods[] = {
+  {"knn_interface", (DL_FUNC) &knn_interface, 7},
+  {NULL, NULL, 0}
+};
+
+extern "C" {
+  void R_init_nearestneighbors(DllInfo *info){
+    R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+    R_useDynamicSymbols(info, FALSE);
   }
+}
