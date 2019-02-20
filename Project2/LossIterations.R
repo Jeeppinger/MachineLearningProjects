c4ffef91497029b53432f933deb1f0fc4f38c21e
Allen <allen.m.clarke@gmail.com>
2019-02-20 15:21:19 -0700

    added some basic files

---
+++ Project2/LossIterations.R
@@ -0,0 +1,18 @@
+#' Gradient Descent alogrithm
+#'
+#'A package that computes the k nearest neighbors 
+#'This funciton calls the c interface in the package to calculate the k nearest neighbors for a given set of data
+#'
+#' @param x.mat  [n x p] matrix of features
+#' @param y.vec label column vector [n]
+#' @param max.iertions max number of steps to take on gradient descent
+#' @param step.size how big of a step to take for each iteration
+#'
+#' @return w.mat a weight vector with one weight per iteration
+#' @export
+#'
+#' @examples
+
+LMSquareLossIterations <- function(x.mat, y.vec,max.iterations ,step.size){
+  
+}
