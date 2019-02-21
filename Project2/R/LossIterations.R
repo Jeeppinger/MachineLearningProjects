5c16219905b968ddddddff4ecf3a60c91e947a09
Allen <allen.m.clarke@gmail.com>
2019-02-21 10:11:59 -0700

    trying to code in R instead of c++

--- Project2/R/LossIterations.R
+++ Project2/R/LossIterations.R
@@ -1,7 +1,7 @@
 #' Linear square loss
 #'
-#'A package that computes the k nearest neighbors 
-#'This funciton calls the c interface in the package to calculate the k nearest neighbors for a given set of data
+#'A package that computes information about linearmodels and regression 
+#'This funciton calls the c interface in the package to calculate the square loss for a given set of data
 #'
 #' @param x.mat  [n x p] matrix of features
 #' @param y.vec label column vector [n]
@@ -14,8 +14,55 @@
 #' @examples
 
 LMSquareLossIterations <- function(x.mat, y.vec,max.iterations ,step.size){
-  w.mat <- .C("loss_interface", as.double(x.mat), as.double(y.vec), as.integer(max.iterations),
-                    as.double(step.size), PACKAGE="linearmodels")
+  #w.mat <- .C("squareloss_interface", as.double(x.mat), as.double(y.vec), as.integer(max.iterations),
+   #           as.double(step.size), PACKAGE="linearmodels")
+    x.mat <- x.mat[,-1] #need to scale x.mat to so standard dev = 1, mean = 0
+    num.row <- dim(x.mat)[1]
+    num.col <- dim(x.mat)[2]
+    
+    mean.vec <- colMeans(x.mat)
+    
+    x.scaled.vec <- sqrt(rowSums((t(x.mat) - mean.vec)^2) / num.row)
+    x.scaled.mat <- diag(num.feature) * (1/x.scaled.vec)
+    
+    x.std.mat <- (t(x.mat) - mean.vec) / x.scaled.vec
+    slope.mat <- matrix(c(rep(0, num.col * max.iterations), num.col, max.iterations)) 
+    
+    # loop to get the slope matrix
+    for (iter.index in (1:max.iterations)){
+      if (iter.index == 1){
+        mean.loss.temp.vec <- (2 * t(x.std.mat) %*% 
+                                 (x.std.mat %*% slope.mat[,1])) / num.train
+        slope.vec.temp <- slope.mat[,1] - step.size * mean.loss.temp.vec
+      }else{
+        mean.loss.temp.vec <- (2 * t(x.std.mat) %*% (x.std.mat %*% slope.mat[,iter.index - 1])) / num.train
+        slope.vec.temp <- slope.mat[,iter.index - 1] - step.size * mean.loss.temp.vec
+      }
+      slope.mat[,iter.index] = slope.vec.temp
+      
+    }
+    itercept <- -t(slope.mat) %*% x.std.mat %*% mean.vec #m x 1
+    slope.mat <- t(slope.mat) %*% x.std.mat  #m x (p-1)
+    w.mat <- rbind(t(itercept),t(slope.mat)) #p x m
+    return(w.mat)
+  }
+  
+
+#' Linear logistic loss
+#' This funciton calls the c interface in the package to calculate the logistic loss for a given set of data
+#'
+#' @param x.mat  [n x p] matrix of features
+#' @param y.vec label column vector [n]
+#' @param max.iterations max number of steps to take on gradient descent
+#' @param step.size how big of a step to take for each iteration
+#'
+#' @return w.mat a weight vector with one weight per iteration
+#' @export
+#'
+#' @examples
+LMLogisticLossIterations <- function(x.mat, y.vec,max.iterations ,step.size){
+  w.mat <- .C("logisticloss_interface", as.double(x.mat), as.double(y.vec), as.integer(max.iterations),
+              as.double(step.size), PACKAGE="linearmodels")
   
 }
 #TODO: set up more functions like above by friday
