7fe585015764c02a2b6bf50b2e5139333e7898b2
Allen <allen.m.clarke@gmail.com>
2019-03-08 19:28:33 -0700

    almost done?

--- Project2/R/L2LossPenalties.R
+++ Project2/R/L2LossPenalties.R
@@ -19,12 +19,12 @@ LMSquareLossL2penalties <- function(x.mat, y.vec, penalty.vec) {
   
   #first scale x
   x.scaled.mat <- scale(x.mat)
-  
+  total.mat <- matrix(c(rep(0, ncol(x.mat) * length(penalty.vec)),ncol(x.mat),length(penalty.vec)))
   slope.mat <- matrix(c(rep(0, ncol(x.mat) * length(penalty.vec)),ncol(x.mat),length(penalty.vec)))
   
   for (current.index in seq(length(penalty.vec))) {#seq is a sequence that counts up to length of penalty vector
     #need initial weight vector
-    optimal.weight.vec <- LMSquareLossL2(x.scaled.mat,y.vec = y.vec,penalty.vec[current.index])
+    optimal.weight.vec <- LMSquareLossL2(x.scaled.mat,y.vec = y.vec, penalty.vec[current.index], 1, slope.mat)
     total.mat[, current.index] <- optimal.weight.vec
   }
   
