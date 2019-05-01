c4ec04bb1430676c89161163db31795e43e9fbf9
Allen <allen.m.clarke@gmail.com>
2019-05-01 12:43:00 -0700

    finished the functions and checks? Needs testing

--- Project4/LinearModelL1.R
+++ Project4/LinearModelL1.R
@@ -1,5 +1,4 @@
-#'Linear Model algorithm
-#'
+
 #' @param x.scaled.mat matrix of size [n x p]
 #' @param y.vec vector of size n
 #' @param penalty non negative numeric scalar
@@ -14,49 +13,96 @@
 LinearModelL1 <- function(x.scaled.mat, y.vec, penalty, opt.thresh, initial.weight.vec, step.size=.5) {
   #1/n ∑i=1^n L[w^T x_i + b, y_i] + penalty * ||w||_1
   #trying to use the cost function written above
-  
-  #first we are going to declare all of our functions that we need 
-  sigmoid<- function(z){
-    1/(1+exp(-z)) 
-  }
-  grad.loss<-function(w.vec){
-    #binary classification so logistic loss
-    pred.vec <- x.sc %*% w
-    prob.vec <- sigmoid(-pred.vec * y.tilde)
-    .grad.vec <- - t(x.sc) %*% (y.tilde  * prob.vec)
-  }
-  soft<- function (w, lambda){
-    sign(w)*positive.part(abs(w) - lambda)
-  }
-  positive.part<-function(x){
-    ifelse(x > 0, x, 0)
-  }
-
-  
-  #wrap this whole piece in a while loop until we have hit the optimal thresh
-  w <- rep(0, l=ncol(x.scaled.mat)) 
-  b<- 0
-  d.vec <- -grad.loss(w)
-  u.vec<- w+step.size*d.vec
-  
-  lambda <- 5
-  w <- soft(u.vec, step.size*lambda)
-  
-  
-  
-}
-
-#' @param x.mat matrix of size [n x p]
-#' @param y.vec vector of size n
-#' @param penalty.vec vector of decreasing penalty values
-#'
-#' @return optimal.weight.vector
-
-LinearModelL1penalties <- function(x.mat, y.vec, penalty.vec) {
-  #first scale x mat
-  #then loop over pentalty values
-  #finally unscale the optimal weight vector
-}
+      # Check type and dimension
+    if (!all(is.numeric(x.scaled.mat), is.matrix(x.scaled.mat))) {
+      stop("x.scaled.mat must be a numeric matrix")
+    }
+    
+    if (!all(is.numeric(y.vec),is.vector(y.vec),length(y.vec) == nrow(x.scaled.mat))) {
+      stop("y.vec must be a numeric vector")
+    }
+    
+    if (!all(is.numeric(penalty), length(penalty) == 1, penalty >= 0)) {
+      stop("penalty must be a numeric scalar > 0")
+    }
+    
+    if (!all(is.numeric(opt.thresh),length(opt.thresh) == 1, opt.thresh > 0)) {
+      stop("opt.thresh must be a numeric scalar > 0")
+    }
+    
+    if (!all(is.numeric(initial.weight.vec), is.vector(initial.weight.vec),length(initial.weight.vec) == ncol(x.scaled.mat) + 1)) {
+      stop("initial.weight.vec must be a numeric vector")
+    }
+    
+    # Initializing functions
+    sigmoid<- function(z){
+      1/(1+exp(-z)) 
+    }
+    grad.loss<-function(w.vec){
+      #binary classification so logistic loss
+      pred.vec <- x.sc %*% w
+      prob.vec <- sigmoid(-pred.vec * y.tilde)
+      grad.vec <- - t(x.sc) %*% (y.tilde  * prob.vec)
+    }
+    positive.part<-function(x){
+      ifelse(x > 0, x, 0)
+    }
+    soft<- function (w, lambda){
+      sign(w)*positive.part(abs(w) - lambda)
+    }
+    l1opt<-function(w.vec, d){
+      ifelse(w.vec==0, 
+             positive.part(abs(d) - lambda), 
+             abs(d-sign(w.vec) * lambda))
+    }
+    
+    
+    
+    is.binary <- ifelse(y.vec %in% c(0, 1), TRUE, FALSE)
+    max.iteration <- 10000L
+    
+    if (is.binary) {
+      y.vec <- ifelse(y.vec == 0, -1, 1)
+    }
+    
+    n.cols <- ncol(x.scaled.mat)   # p 
+    n.rows <- nrow(x.scaled.mat)  # n 
+    
+    x.train <- cbind(1,x.scaled.mat) # n x (p+1)    
+    w.vec <- rnorm(n.cols) # p x 1
+    intercept <- rnorm(1)
+    
+    while (1) {
+      if (is.binary) {
+        # do logistic
+        w.gradient.vec <-t(x.train) %*% (y.vec / (1 + exp(y.vec * (x.train %*% w.vec + rep(1,n.rows) * intercept))))
+        
+        intercept.gradient <- t(rep(1,n.rows)) %*% (y.vec / (1 + exp(y.vec * (x.train %*% w.vec + rep(1,n.rows) * intercept))))
+        
+        u.vec <- w.vec + step.size * w.gradient.vec / n.rows
+        intercept <- intercept + step.size * intercept.gradient / n.rows
+        w.vec <- soft(u.vec, step.size * penalty)
+      } else{
+        # do linear square loss
+        w.gradient.vec <- -t(x.train) %*% (x.train %*% w.vec + rep(1,n.rows) * intercept - y.vec)
+        
+        intercept.gradient <- -t(rep(1,n.rows)) %*% (x.train %*% w.vec + rep(1,n.rows) * intercept - y.vec)
+        
+        intercept <- intercept + step.size * intercept.gradient / n.rows
+        u.vec <- w.vec + step.size * w.gradient.vec / n.rows
+        w.vec <- soft(u.vec, step.size * penalty)
+      }
+      
+      temp.w.vec <- c(intercept, w.vec) 
+      if (all(positive.part(w.gradient.vec[w.vec==0] - penalty) < opt.thresh, 
+              positive.part(w.gradient.vec[w.vec!=0] - sign(w.vec[w.vec!=0]) * penalty) < opt.thresh,
+              abs(intercept.gradient) < opt.thresh))
+        break;
+    }
+    
+    w.vec <- c(intercept, w.vec)
+    return(w.vec)
+}  
 
 #' @param x.mat matrix of size [n x p]
 #' @param y.vec vector of size n
---
+++ Project4/LinearModelL1CV.R
@@ -0,0 +1,100 @@
+#' Linear Model with L1 regularization using cross validation
+#'
+#' This algorithm splits the data into several folds and apply LinealModelL1penalites to each fold
+#'
+#' @param x.mat a numeric feature matrix of size n x p
+#' @param y.vec a numeric labe vector of length nrow(x.mat)
+#' @param fold.vec a numeric vector of lenght nrow(x.mat)
+#' @param n.folds a positive integer indicate number of folds, default is 5
+#' @param penalty.vec a non-negative numeric decreasing penalty vector, default is 1 to 0.1 with 0.1 decreament
+#' @param step.size a positive numeric value, default is 0.1
+#'
+#' @return
+#' @export
+#'
+#' @examples
+LinearModelL1CV <-function(x.mat,y.vec,fold.vec = sample(rep(1:n.folds, l = length(y.vec))),n.folds = 5L,penalty.vec = seq(1, 0.1, -0.1),step.size = 0.1) {
+    # Check types
+  
+    if (!all(is.numeric(x.mat), is.matrix(x.mat))) {
+      stop("x.mat must be a numeric matrix")
+    }
+    
+    if (!all(is.numeric(y.vec),is.vector(y.vec), length(y.vec) == nrow(x.mat))) {
+      stop("y.vec must be a numeric vector")
+    }
+    
+    
+    if (!all(is.numeric(fold.vec),is.vector(fold.vec),length(fold.vec) == nrow(x.mat))) {
+      stop("fold.vec must be a numeric vector")
+    }
+    
+    if (!all(is.vector(penalty.vec),is.numeric(penalty.vec),penalty.vec >= 0,diff(penalty.vec) < 0)) {
+      stop("penalty.vec must be a decreasing numeric vector")
+    }
+    
+    # Set up 
+    is.binary <- ifelse((all(y.vec %in% c(0, 1))), TRUE, FALSE)
+    
+    n.cols <- ncol(x.mat)
+    train.loss.mat <- matrix(0, nrow = n.folds, ncol = length(penalty.vec))
+    validation.loss.mat <- matrix(0, nrow = n.folds, ncol = length(penalty.vec))
+    
+    # Iterating over each fold
+    for (fold.idx in seq(n.folds)) {
+      train.vec <- (fold.vec != fold.idx)
+      
+      set.list <- list(train = train.vec, validation = (!train.vec))
+      for (set.name in names(set.list)) {
+        index <- get(set.name, set.list)
+        
+        W.mat <-
+          LinearModelL1penalties(x.mat, y.vec, penalty.vec, step.size)
+        predict <- cbind(1, x.mat[index,]) %*% W.mat
+        
+        if (is.binary) {
+          # Do 0-1 loss
+          predict <- ifelse(predict > 0.5, 1, 0)
+          loss.vec <-
+            colMeans((ifelse(predict == y.vec[get(set.name, set.list)], 0, 1)))
+        } else{
+          # Do square loss
+          loss.vec <-
+            colMeans((predict - y.vec[get(set.name, set.list)]) ^ 2)
+        }
+        
+        if (set.name == "train") {
+          train.loss.mat[fold.idx, ] <- loss.vec
+        } else{
+          validation.loss.mat[fold.idx, ] <- loss.vec
+        }
+      }
+    }
+    mean.train.loss.vec <- colMeans(train.loss.mat)
+    mean.validation.loss.vec <- colMeans(validation.loss.mat)
+    selected.penalty.index <- which.min(mean.validation.loss.vec)
+    
+    weight.vec <- LinearModelL1penalties(x.mat, y.vec, penalty.vec)[, selected.penalty.index]
+    
+    predict <- function(testx.mat) {
+      # Check inputs here
+      if (!all(is.numeric(testx.mat),is.matrix(testx.mat), ncol(testx.mat) == n.cols)) {
+        stop("testx.mat must be a numeric matrix ")
+      }
+      
+      prediction.vec <- cbind(1, testx.mat) %*% weight.vec
+      
+      return(prediction.vec)
+    }
+    
+    result.list <- list(
+      mean.validation.loss.vec = mean.validation.loss.vec,
+      mean.train.loss.vec = mean.train.loss.vec,
+      penalty.vec = penalty.vec,
+      selected.penalty = penalty.vec[selected.penalty.index],
+      weight.vec = weight.vec,
+      predict = predict
+    )
+    
+    return(result.list)
+  }
